d3ea3fba77f0388f82ad24e678b90ee6b6de7e05
Carolyn Mason <cmason@digitalocean.com>
2021-10-25 18:12:19 -0700

    getting more complex

--- app.py
+++ app.py
@@ -26,7 +26,69 @@ app = dash.Dash(
     __name__,
     server=server
 )
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
+])
 
-@app.route("/")
-def hello_world():
-    return render_template("index.html")
+index_page = html.Div([
+    html.A("Get Some Data!",
+           href="https://www.strava.com/oauth/authorize?client_id=32737&response_type=code&redirect_uri=https://www.giraffesinaboat.com/exchange_token&approval_prompt=force&scope=activity:read_all")
+])
+
+local_timezone = datetime.datetime.now(datetime.timezone(datetime.timedelta(0))).astimezone().tzinfo
+dend = pd.Timestamp.today() + pd.DateOffset(years=1)
+datelist = pd.date_range(start='1/1/2009', end= dend, freq='Y', tz=local_timezone)
+maxmarks = len(datelist) - 1
+DLIST = pd.DatetimeIndex(datelist).normalize()
+TAGS = {}
+for idx,item in enumerate(DLIST):
+    TAGS[idx] = (item + DateOffset(months=1)).strftime('%Y')
+
+page_1_layout = html.Div([
+    dcc.Loading(
+        id="loading-1",
+        type="default",
+        fullscreen=True,
+        children=[html.Div(dcc.Store(id='memory')),
+                  html.Div(dcc.Store(id='local', storage_type='local')),
+                  html.Div(dcc.Store(id='session', storage_type='session')),
+                  html.Div(dcc.Dropdown(
+                           id='dropdown',
+                           multi=True,
+                           placeholder="Select Activity Type")),
+                  html.Div(dcc.Dropdown(
+                           id='dropdown2',
+                           multi=True,
+                           placeholder="Select Gear"
+                  )),
+                  html.Div(dcc.RangeSlider(
+                           id='time-slider',
+                           updatemode='mouseup',
+                           count=1,
+                           min=0,
+                           max=maxmarks,
+                           step=1,
+                           value=[0,maxmarks],
+                           marks=TAGS,
+                           pushable=1
+                  ))]
+    ),
+    html.Div(id='test',
+             style={'width': '100%',
+                    'height': '600px',
+                    'margin': 'auto',
+                    'display': 'block'})
+])
+
+@app.callback(
+    Output('page-content', 'children'),
+    [Input('url', 'pathname'),
+     Input('url', 'href')]
+)
+def display_page(pathname, href):
+    if pathname == '/exchange_token':
+        return page_1_layout
+    else:
+        return index_page
+
